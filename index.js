1d15b42c035139c0ecf46477a277cd351c6434a3
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-08-28 18:13:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(arr, name) {
+    return arr.filter((el) => el.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(arr, name) {
+    return arr.filter((el) => el[0] === name[0]);
+}
+
+const driver = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(arr, name) {
+    return arr.filter((el) => el.name === name);;
+}
